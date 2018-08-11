cf1d42b87c3d59b8971ae06f0abd64e119ac5abf
Mashhood Rastgar <mashhoodr@gmail.com>
2018-08-11 15:52:16 +0500

    basic functionality working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '../../node_modules/@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  query = '';
   title = 'animation-demo';
+  images = [
+    {
+      uri: '1.jpg',
+      heading: 'Amazing Waterfall',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '2.jpg',
+      heading: 'Scenic Mountain',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '3.jpg',
+      heading: 'Forest Waterfall',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '4.jpg',
+      heading: 'Grassy Fields',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '5.jpg',
+      heading: 'Forest Lake',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '6.jpg',
+      heading: 'Canyons Maze',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '7.jpg',
+      heading: 'Beautiful Garden',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '8.jpg',
+      heading: 'Rocky Beach',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    },
+    {
+      uri: '9.jpg',
+      heading: 'Sunset at Beach',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consecteturadipiscing elit.'
+    }
+  ];
+  closeResult;
+
+  constructor(private modalService: NgbModal) {
+
+  }
+
+  open(content) {
+    this.modalService.open(content, {
+      ariaLabelledBy: 'modal-basic-title',
+      size: 'lg',
+      windowClass: 'dark',
+      centered: true
+    }).result.then((result) => {
+      console.log(`Closed with: ${result}`);
+    }, (reason) => {
+      console.log(`Dismissed ${reason}`);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '../../node_modules/@angular/forms';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    NgbModalModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
